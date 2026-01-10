fa3a2f30958cfd6cf3f2cc5a8823cac39c259981
Yuriy Mamaev (execut) <sexecut@gmail.com>
2026-01-10 18:27:07 +0300

    ddd-story-6

--- steps/domain/manufacturer_organization_name.go
+++ steps/domain/manufacturer_organization_name.go
@@ -1,9 +1,20 @@
 package domain
 
+import "errors"
+
+var (
+    ErrManufacturerOrganizationNameWrongLen = errors.New(
+        "название организации производителя должно быть до 255 символов в длину")
+)
+
 type ManufacturerOrganizationName struct {
     Name string
 }
 
 func NewManufacturerOrganizationName(name string) (ManufacturerOrganizationName, error) {
+    if len(name) > 255 || len(name) == 0 {
+        return ManufacturerOrganizationName{}, ErrManufacturerOrganizationNameWrongLen
+    }
+
     return ManufacturerOrganizationName{Name: name}, nil
 }
